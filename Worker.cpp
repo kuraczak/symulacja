6ce677beffa82a3ec8928cdddd751745b1a78639
Szymon Kuczaty <skuczaty@gmail.com>
2017-12-28 13:35:48 +0100

    add Worker class

---
+++ Worker.cpp
@@ -0,0 +1,18 @@
+/*
+ * Worker.cpp
+ *
+ *  Created on: 28 gru 2017
+ *      Author: kuraczak
+ */
+
+#include "Worker.h"
+
+Worker::Worker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Worker::~Worker() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Worker.h
@@ -0,0 +1,17 @@
+/*
+ * Worker.h
+ *
+ *  Created on: 28 gru 2017
+ *      Author: kuraczak
+ */
+
+#ifndef WORKER_H_
+#define WORKER_H_
+
+class Worker {
+public:
+	Worker();
+	virtual ~Worker();
+};
+
+#endif /* WORKER_H_ */

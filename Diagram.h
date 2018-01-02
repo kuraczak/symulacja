ff502efc54b568e6600cb956c504defc901345cd
kuraczak <skuczaty@gmail.com>
2018-01-02 19:01:11 +0100

    message

--- Diagram.h
+++ Diagram.h
@@ -5,13 +5,23 @@
  *      Author: kuraczak
  */
 
+
 #ifndef DIAGRAM_H_
 #define DIAGRAM_H_
+
+#include <vector>
+#include "Ramp.h"
+#include "Worker.h"
+#include "Storehouse.h"
 
 class Diagram {
 public:
 	Diagram();
 	virtual ~Diagram();
+	std::vector<Ramp> ramps;
+	std::vector<Worker> workers;
+private:
+
 };
 
 #endif /* DIAGRAM_H_ */
